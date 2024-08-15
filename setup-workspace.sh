b698a38948de26e7f869184590aba74741f76957
Noppong Trongjitsakul <107835854+noppongtr@users.noreply.github.com>
2024-08-15 11:13:53 +0700

    Update setup-workspace.sh
    fix error and update new param

--- setup-workspace.sh
+++ setup-workspace.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-dirSource=$1
-echo ">>>> source directory: $1 <<<<";
 
-mkdir bin pkg src
-cd src
+echo ">>>> source directory: $1 | new directory: $2 <<<<";
 
-mkdir go.tn.io && cd go.tn.io
-mkdir framework && cd framework
+mkdir -p $2/bin $2/pkg $2/src
+mkdir -p $2/src/go.tn.io/framework
 
-mv ../../../$1 app-server/
+mv $1 $2/src/go.tn.io/framework/app-server
+
 echo " ************** Generate Directory Finish ************** "
